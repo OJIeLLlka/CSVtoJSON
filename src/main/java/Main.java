39c5ce53afc748196f96a79abb568ff54f1d0b37
OJIeLLlka <olbastnie@gmail.com>
2022-03-23 18:40:18 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+public class Main {
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) throws IOException {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        }
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String JSONstr, String fileName) throws IOException {
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(JSONstr);
+            file.flush();
+        }
+    }
+
+        public static void main(String[] args) throws IOException {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "data.json");
+    }
+}
